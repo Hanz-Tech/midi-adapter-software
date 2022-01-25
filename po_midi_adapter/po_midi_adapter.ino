9d2810b6007f19997daaaea85fc1d609a9a35058
Han Zhao <4143538+HanYangZhao@users.noreply.github.com>
2022-01-25 15:47:17 -0500

    version bump to 2.3.5

--- po_midi_adapter/po_midi_adapter.ino
+++ po_midi_adapter/po_midi_adapter.ino
@@ -6,7 +6,7 @@
 #include "po_control.h"
 #define LEN(arr) ((uint8_t) (sizeof (arr) / sizeof (arr)[0]))
 
-#define FIRMWARE_VERSION "2.3.4-beta"
+#define FIRMWARE_VERSION "2.3.5"
 
 // Create the Serial MIDI portsm
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, MIDI1);
